f54e4967924e0eab0a458a7d36cf01f81311d736
Pradhansumit <pradhansumit026@gmail.com>
2025-05-29 14:00:06 +0000

    Focus on search bar when control + k pressed down.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useRef } from "react";
+
 const Header = () => {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.ctrlKey && e.key.toLowerCase() === "k") {
+        e.preventDefault();
+        inputRef.current?.focus();
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
   return (
     <div className="p-5 shadow-lg bg-slate-800 mb-5">
       <div className="flex items-center justify-center">
-        <label className="input sm:8/12 md:w-4/12">
+        <label className="input md:w-4/12">
           <svg
             className="h-[1em] opacity-50"
             xmlns="http://www.w3.org/2000/svg"
@@ -19,8 +34,13 @@ const Header = () => {
               <path d="m21 21-4.3-4.3"></path>
             </g>
           </svg>
-          <input type="search" className="grow" placeholder="Search" />
-          <kbd className="kbd kbd-sm">⌘</kbd>
+          <input
+            ref={inputRef}
+            type="search"
+            className="grow"
+            placeholder="Search"
+          />
+          <kbd className="kbd kbd-sm">Ctrl</kbd>
           <kbd className="kbd kbd-sm">K</kbd>
         </label>
       </div>
